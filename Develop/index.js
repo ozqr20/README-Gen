51bfa5e4ec2503e56bbd72dc81380e6ed541f7d5
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-08-08 21:49:58 -0400

    generateMarkdown

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,81 @@
-// TODO: Include packages needed for this application
+/*
+AS A developer
+I WANT a README generator
+SO THAT I can quickly create a professional README for a new project
+
+GIVEN a command-line application that accepts user input
+WHEN I am prompted for information about my application repository
+THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+WHEN I enter my project title
+THEN this is displayed as the title of the README
+WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+WHEN I choose a license for my application from a list of options
+THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+WHEN I enter my GitHub username
+THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+WHEN I enter my email address
+THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+WHEN I click on the links in the Table of Contents
+THEN I am taken to the corresponding section of the README
+
+*/
+
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'username',
+            message: "Enter your Github username: ",
+            validate: usernameInput => {
+                if(usernameInput){
+                    return true;
+                } else {
+                    console.log('Please provide a Github username');
+                    return false;
+                }
+            }
+
+        },
+        {
+            type: 'input',
+            name:'title',
+            message: 'What is your project title? (Required) ',
+            validate: nameInput => {
+                if(nameInput){
+                    return true;
+                } else {
+                    console.log('Please provide a name for your project');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'What is the description of your project (Required),  use the following questions as guide \n - What was your motivation? \n -Why did you build this specific project? \n - What problem does it solve? \n What did you learn? \n',
+
+            validate: descriptionInput => {
+                if(descriptionInput){
+                    return true;
+                } else {
+                    console.log('Please provide a description for your project')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'Installation',
+            message: 'Please provide '
+
+        }
+    ])
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,57 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return ''
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return ''
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return ''
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${data.description}
+  # Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Licence](#licence)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [Credits](#credits)
+  
+  # Installation
+  ${data.installation}
 
+  # Usage
+  ${data.usage}
+
+  # Licence
+  ${data.license}
+
+  # Contributing
+  ${data.contribute}
+  
+  # Tests
+  ${data.tests}
+
+  # Questions
+  ${data.qa}
+  Any questions please email me at ${data.email}
+
+  # Credits
+  Github: [${data.usarname}](${data.url})
+
+  Copyright ${data.name}. All rights Reserved. 
 `;
 }
 
