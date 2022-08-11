3668acc14c1d996ca9852291ac046e4d45f1f028
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-08-10 21:38:51 -0400

    Licence section added

--- Develop/index.js
+++ Develop/index.js
@@ -1,29 +1,8 @@
-/*
-AS A developer
-I WANT a README generator
-SO THAT I can quickly create a professional README for a new project
-
-GIVEN a command-line application that accepts user input
-WHEN I am prompted for information about my application repository
-THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-WHEN I enter my project title
-THEN this is displayed as the title of the README
-WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-WHEN I choose a license for my application from a list of options
-THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-WHEN I enter my GitHub username
-THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-WHEN I enter my email address
-THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-WHEN I click on the links in the Table of Contents
-THEN I am taken to the corresponding section of the README
-
-*/
 
 const inquirer = require('inquirer');
 const fs = require('fs');
 const markDown = require('./utils/generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 
 // TODO: Create an array of questions for user input
@@ -41,7 +20,6 @@ const questions = () => {
                     return false;
                 }
             }
-
         },
         {
             type: 'input',
@@ -73,21 +51,76 @@ const questions = () => {
                     return false;
                 }
             }
-        }
-        // {
-        //     type: 'input',
-        //     name: 'Installation',
-        //     message: 'Please provide '
-
-        // }
+        },
+        {
+            type: 'input',
+            name: 'Installation',
+            message: 'Please provide the installation instructions ',
+            validate: instalationInput => {
+                if(instalationInput){
+                    return true;
+                } else {
+                    console.log('Please provide the installation instructions')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'Usage Information',
+            message: 'Please provide the usage information ',
+            validate: usageInput => {
+                if(usageInput){
+                    return true;
+                } else {
+                    console.log('Please provide the usage information')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'Choose your licence',
+            message: 'Please the requiere licence ',
+            choices: ['MIT', 'GNU GPLv3', 'GNU AGPLv3', 'GNU LGPLv3', 'Apache', 'Mozilla', 'unlicensed'],
+            validate: licenceInput => {
+                if(licenceInput){
+                    return true;
+                } else {
+                    console.log('Please provide the installation instructions')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'test instructions',
+            message: 'Please enter test instructions ',
+            validate: testInput => {
+                if(testInput){
+                    return true;
+                } else {
+                    console.log('Please provide the installation instructions')
+                    return false;
+                }
+            }
+        } 
     ])
-}
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(`${fileName}`, data, (err) =>
+    err ? console.error(err): console.log('Checked'));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions()
+    .then((data) => {
+        writeToFile('README.md', generateMarkdown(data))
+    }))
+};
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,31 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  return ''
+  var badges = {
+    'Apache 2.0': `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`,
+    'GNU GPLv3':  `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`,
+    'MIT': `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`,
+    'Mozilla': `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`,
+    'GNU AGPLv3': `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`,
+    'GNU LGPLv3': `[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)`,
+    'unlicensed': '',
+  }
+  return badges[license]
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 function renderLicenseLink(license) {
-  return ''
+  var linkLicence = {
+    'Apache 2.0': `https://opensource.org/licenses/Apache-2.0`,
+    'GNU GPLv3': `https://www.gnu.org/licenses/agpl-3.0`,
+    'MIT': `https://opensource.org/licenses/MIT`,
+    'Mozilla': `https://opensource.org/licenses/MPL-2.0`,
+    'GNU AGPLv3': `https://www.gnu.org/licenses/agpl-3.0`,
+    'GNU LGPLv3': `https://www.gnu.org/licenses/lgpl-3.0`,
+    'unlicensed': '',
+  }
+  return linkLicence[license]
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 function renderLicenseSection(license) {
-  return ''
+  return `Licensed using ${license}. You can find more information here ${renderLicenseLink(license)}`
 }
 
 // TODO: Create a function to generate markdown for README
@@ -29,6 +41,7 @@ function generateMarkdown(data) {
   * [Questions](#questions)
   * [Credits](#credits)
   
+  ${renderLicenseBadge(data.license)}
   # Installation
   ${data.installation}
 
@@ -36,7 +49,7 @@ function generateMarkdown(data) {
   ${data.usage}
 
   # Licence
-  ${data.license}
+  ${renderLicenseSection(data.license)}
 
   # Contributing
   ${data.contribute}
@@ -49,9 +62,9 @@ function generateMarkdown(data) {
   Any questions please email me at ${data.email}
 
   # Credits
-  Github: [${data.usarname}](${data.url})
+  Github: https://github.com/${data.usarname}
 
-  Copyright ${data.name}. All rights Reserved. 
+  Copyright ${data.usarname}. All rights Reserved. 
 `;
 }
 
